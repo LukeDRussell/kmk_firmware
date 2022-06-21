7b8bf4d2a4ada07af031f459a510812043085a2a
Luke D Russell <lukedrussell+git@outlook.com>
2022-06-21 20:41:01 +1000

    Add license header.

--- util/mpy_cross_all.py
+++ util/mpy_cross_all.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 
 '''
-This file was vendored from MicroPython to make compiling KMK more feasible on Windows,
-which doesn't have easy access to Make. This file is licensed with the permissive MIT
-license.
+mpy_cross_all.py
+Vendored from MicroPython to make compiling KMK more feasible on Windows.
 https://raw.githubusercontent.com/micropython/micropython/master/tools/mpy_cross_all.py
 
+-----------------------------------------------------------------------------
+The MIT License (MIT)
+
+Copyright (c) 2013-2022 Damien P. George
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+--------------------------------------------------------------------------------
 '''
 
 import argparse
