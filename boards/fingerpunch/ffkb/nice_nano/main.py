a008680354d7e514ddf42bccce749402bf11bf1d
Luke D Russell <lukedrussell+git@outlook.com>
2022-08-29 10:19:59 +1000

    Updates

--- boards/fingerpunch/ffkb/nice_nano/main.py
+++ boards/fingerpunch/ffkb/nice_nano/main.py
@@ -35,51 +35,40 @@ combos.combos = [
     Sequence((KC.LEADER, KC.Y), KC.LCTL(KC.Y)),  # redo
     ]
 
-___ = KC.TRNS
-xxx = KC.NO
+# Convenience variables for the Keymap
+_______ = KC.TRNS
+xxxxxxx = KC.NO
+
+L1_TAB = KC.LT(1, KC.TAB, prefer_hold=True)
+L2_ENT = KC.LT(2, KC.ENT, prefer_hold=True)
 
 OS_LSFT = KC.OS(KC.LSFT)
-TAB_NAV = KC.LT(1, KC.TAB, prefer_hold=True)
-ENT_FN = KC.LT(2, KC.ENT, prefer_hold=True)
-gaming = KC.DF(4)
-no_game = KC.DF(0)
-ESC_G2 = KC.LT(5, KC.ESC, prefer_hold=True)
-
 # fmt: off
 # flake8: noqa
 keyboard.keymap = [
-    [
-        KC.ESC,     KC.Q,   KC.W,   KC.F,   KC.P,   KC.B,           KC.J,   KC.L,   KC.U,   KC.Y,   KC.SCLN,KC.GUI,
-        KC.LCTL,    KC.A,   KC.R,   KC.S,   KC.T,   KC.G,   xxx,    KC.M,   KC.N,   KC.E,   KC.I,   KC.O,   KC.QUOT,
-        KC.LALT,    KC.Z,   KC.X,   KC.C,   KC.D,   KC.V,           KC.K,   KC.H,   KC.COMM,KC.DOT, KC.SLSH,KC.BSLS,
-                    xxx,       KC.LEADER,   OS_LSFT,KC.BSPC,        TAB_NAV,KC.SPACE,ENT_FN,        xxx,
+    [  # 0: Colemak-DH letters
+        KC.ESC,  KC.Q,    KC.W,    KC.F,    KC.P,    KC.B,          KC.J,    KC.L,    KC.U,    KC.Y,    KC.SCLN, KC.LEADER,
+        KC.LCTL, KC.A,    KC.R,    KC.S,    KC.T,    KC.G,          KC.M,    KC.N,    KC.E,    KC.I,    KC.O,    KC.QUOT,
+        KC.LALT, KC.Z,    KC.X,    KC.C,    KC.D,    KC.V,          KC.K,    KC.H,    KC.COMM, KC.DOT,  KC.SLSH, KC.BSLS,
+                 xxxxxxx,          KC.LGUI, OS_LSFT, KC.BSPC,       L1_TAB,    KC.SPACE,L2_ENT,             xxxxxxx,
     ],
-    [
-        KC.TAB,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,            KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.DEL,
-          ___  , KC.LPRN, KC.LEFT, KC.UP,   KC.RGHT,KC.RPRN,   ___  , KC.GRV,  KC.PLUS, KC.EQL,    xxx  ,   xxx  ,   xxx  ,
-          ___  , KC.LBRC, KC.LCBR, KC.DOWN, KC.RCBR, KC.RBRC,          KC.TILD, KC.MINS, KC.UNDS,   xxx  ,   xxx  ,   xxx  ,
-                      ___  ,            ___  ,   ___  , KC.DEL,             ___  ,   ___  ,   ___  ,   ___  ,
+    [  # 1: Nav & Numbers
+        KC.TAB,  KC.N1,   KC.N2,   KC.N3,   KC.N4,   KC.N5,         KC.N6,   KC.N7,   KC.N8,   KC.N9,   KC.N0,   KC.DEL,
+        _______, KC.LPRN, KC.LEFT, KC.UP,   KC.RIGHT,KC.RPRN,       KC.GRV,  KC.PLUS, KC.EQL,  xxxxxxx, xxxxxxx, xxxxxxx,
+        _______, KC.LBRC, KC.LCBR, KC.DOWN, KC.RCBR, KC.RBRC,       KC.TILD, KC.MINS, KC.UNDS, xxxxxxx, xxxxxxx, xxxxxxx,
+                 _______,          _______, _______, KC.DEL,        _______, _______, _______, _______,
     ],
-    [
-        KC.F12,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,            KC.F6,   KC.F7,   KC.F8,   KC.F9,    KC.F10,  KC.F11,
-          ___  , SEQ_REC, SEQ_PLY,   ___  ,   ___  ,   ___  ,   ___  ,   ___  ,   ___  ,   ___  ,   ___  ,    ___  ,   ___  ,
-          ___  , SEQ_STP,   ___  ,   ___  ,   ___  ,   ___  ,            ___  ,   ___  ,   ___  ,   ___  ,    ___  ,   ___  ,
-                  ___  ,            ___   ,   ___  ,   ___  ,            gaming, ___  ,   ___  ,             ___  , 
-    ],
-    [
-        KC.TAB, KC.N1,  KC.N2,  KC.N3,  KC.N4,  KC.N5,          xxx,    xxx,    xxx,    xxx,    xxx,    xxx,
-        KC.LCTL,KC.F1,  KC.LEFT,KC.UP,  KC.RGHT,KC.F2,  xxx,    xxx,    xxx,    xxx,    xxx,    xxx,    xxx,
-        KC.LALT,KC.F3,  KC.F4,  KC.DOWN,KC.F5,  KC.F6,          xxx,    xxx,    xxx,    xxx,    xxx,    xxx,
-                xxx,            ESC_G2, KC.SPC, KC.LSFT,                no_game,xxx,    ENT_FN, xxx,
-    ],
-    [
-        ___,    KC.N6,  KC.N7,  KC.N8,  KC.N9,  KC.N0,          ___,    ___,    ___,    ___,    ___,    ___,
-        ___,    ___,    ___,    ___,    ___,    ___,    ___,    ___,    ___,    ___,    ___,    ___,    ___,
-        ___,    ___,    ___,    ___,    ___,    ___,            ___,    ___,    ___,    ___,    ___,    ___,
-                ___,            ___,    ___,    ___,            ___,    ___,    ___,      ___
+    [  # 2: F-row & Board Functions
+        KC.F12,  KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,         KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,  KC.F11,
+        _______, SEQ_REC, SEQ_PLY, _______, _______, _______,       _______, _______, _______, _______, _______,
+        _______, SEQ_STP, _______, _______, _______, _______,       _______, _______, _______, _______, _______, _______,
+                 _______,          _______, _______, _______,       _______, _______, _______,          _______, 
     ],
 ]
 # fmt: on
 
 if __name__ == '__main__':
+    print()
+    print("KMK starting...")
+    print()
     keyboard.go()
