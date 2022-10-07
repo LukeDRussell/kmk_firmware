84740c70d1ea516affb2bb18fea4aee5092fe619
Luke D Russell <lukedrussell+git@outlook.com>
2022-10-07 18:00:00 +1000

    New boot.py to remove USB storage appearing on OS.

---
+++ boards/fingerpunch/ffkb/nice_nano/boot.py
@@ -0,0 +1,25 @@
+import supervisor
+import board
+import digitalio
+import storage
+import usb_cdc
+import usb_hid
+
+# Why is this here?
+supervisor.set_next_stack_limit(4096 + 4096)
+
+# If this key is held during boot, don't run the code which hides the storage and disables serial
+col = digitalio.DigitalInOut(board.P1_06)
+row = digitalio.DigitalInOut(board.P1_08)
+
+col.switch_to_output(value=True)
+row.switch_to_input(pull=digitalio.Pull.DOWN)
+
+if not row.value:
+    storage.disable_usb_drive()
+    # Equivalent to usb_cdc.enable(console=False, data=False)
+    usb_cdc.disable()
+    usb_hid.enable(boot_device=1)
+
+row.deinit()
+col.deinit()
